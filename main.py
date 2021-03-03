1175abbf48884957074e074986f4bc72490ed574
Michael Odermatt <michaelodermatt@outlook.com>
2021-03-03 08:38:10 -0800

    new json model and trained thera-bot

--- main.py
+++ main.py
@@ -21,14 +21,15 @@ def chat():
 		if inp.lower() == "quit":
 			break
 
-		results = model.predict([bag_of_words(inp, words)])
+		results = model.predict([bag_of_words(inp, words)])[0]
 		results_index = numpy.argmax(results)
 		tag = labels[results_index]
+		if results[results_index] > 0.7:
+			for tg in data["intents"]:
+				if tg['tag'] == tag:
+					responses = tg['responses']
 
-		for tg in data["intents"]:
-			if tg['tag'] == tag:
-				responses = tg['responses']
-
-		print(random.choice(responses))
-
+			print(random.choice(responses))
+		else:
+			print("Sorry, I'm not sure what you are trying to say. Could you please try again.")
 chat()
